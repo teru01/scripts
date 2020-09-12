01e192d174fc1f4d5efe830f15e5d5e66a438f94
teruya <teru0x01.sheep@gmail.com>
2020-09-12 11:41:11 +0900

    add dispost

--- summarize_npr.sh
+++ summarize_npr.sh
@@ -13,3 +13,6 @@ sudo cp -a /var/log/mysql/mysql-slow.log $dir
 # パーミッションまわり調整
 sudo chown -R $USER:$GROUPS $dir
 pt-query-digest $dir/mysql-slow.log > $dir/digest.txt
+
+./dispost $dir/digest.txt
+./dispost $dir/result.txt
