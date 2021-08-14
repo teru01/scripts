8a4f5da624aa47515e68db6808646ec6e3daf04d
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2021-08-14 13:00:15 +0900

    fix

--- setup_npr.sh
+++ setup_npr.sh
@@ -7,12 +7,10 @@ cd "${0%/*}"
 dir=$(date +%Y-%m-%d-%H-%M-%S)
 mkdir $dir
 touch $dir/$dir
-cp -a nginx.conf $dir
-sudo nginx -s stop
-sudo nginx -p $dir -c nginx.conf
 
-sudo cp -a mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo rm /var/log/mysql/mysql-error.log /var/log/mysql/mysql-slow.log
-sudo systemctl restart mysql
+sudo rm /var/log/nginx/access.log
+sudo rm /var/log/mysql/slow.log
+sudo systemctl restart nginx
+sudo systemctl restart mysqld
 
 echo $dir
--- summarize_npr.sh
+++ summarize_npr.sh
@@ -5,14 +5,12 @@ set -ux
 cd "${0%/*}"
 
 dir="${1%/}"
+sudo cp -a /var/log/mysql/slow.log $dir
+sudo cp -a /var/log/nginx/access.log $dir
 cp kataribe.toml $dir
 cat $dir/access.log | kataribe > $dir/result.txt
-sudo cp -a /var/log/mysql/mysql-error.log $dir
-sudo cp -a /var/log/mysql/mysql-slow.log $dir
 
 # パーミッションまわり調整
 sudo chown -R $USER:$GROUPS $dir
-pt-query-digest $dir/mysql-slow.log > $dir/digest.txt
+pt-query-digest $dir/slow.log > $dir/digest.txt
 
-./dispost $dir/digest.txt
-./dispost $dir/result.txt
