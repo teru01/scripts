53a8ef285390d4d39d17a49704a9ce4dd8a18a0c
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2021-08-14 15:30:07 +0900

    update

--- setup_npr.sh
+++ setup_npr.sh
@@ -9,7 +9,7 @@ mkdir $dir
 touch $dir/$dir
 
 sudo rm /var/log/nginx/access.log
-sudo rm /var/log/mysql/slow.log
+sudo rm /var/log/mysql/mysql-slow.log
 sudo systemctl restart nginx
 sudo systemctl restart mysqld
 
--- summarize_npr.sh
+++ summarize_npr.sh
@@ -5,11 +5,11 @@ set -ux
 cd "${0%/*}"
 
 dir="${1%/}"
-sudo cp -a /var/log/mysql/slow.log $dir
+sudo cp -a /var/log/mysql/mysql-slow.log $dir
 sudo cp -a /var/log/nginx/access.log $dir
 cp kataribe.toml $dir
 cat $dir/access.log | kataribe > $dir/result.txt
 
 # パーミッションまわり調整
 sudo chown -R $USER:$GROUPS $dir
-pt-query-digest $dir/slow.log > $dir/digest.txt
+pt-query-digest $dir/mysql-slow.log > $dir/digest.txt
